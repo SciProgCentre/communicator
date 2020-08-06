475086242b80d9361fd21eda780e84e5172d53b4
Iaroslav <postovalovya@gmail.com>
2020-08-06 14:47:49 +0700

    Delete old stubs, implement simple client class

--- communicator-userapi/src/jvmMain/kotlin/scientifik.communicator.userapi.transport/ClientTransport.kt
+++ communicator-userapi/src/jvmMain/kotlin/scientifik.communicator.userapi.transport/ClientTransport.kt
@@ -1,9 +1,10 @@
 package scientifik.communicator.userapi.transport
 
-import org.zeromq.SocketType
-import org.zeromq.ZContext
-import org.zeromq.ZLoop
-import org.zeromq.ZMsg
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Deferred
+import org.zeromq.*
+import scientifik.communicator.api.Payload
+import scientifik.communicator.userapi.scientifik.communicator.userapi.transport.getUuid
 import scientifik.communicator.userapi.scientifik.communicator.userapi.transport.putUuid
 import java.io.Closeable
 import java.nio.ByteBuffer
@@ -11,14 +12,38 @@ import java.util.*
 
 internal class ClientTransport : Closeable {
     private val context: ZContext = ZContext()
-    private val dealer: org.zeromq.ZMQ.Socket = context.createSocket(SocketType.DEALER)
+    private val dealer: ZMQ.Socket = context.createSocket(SocketType.DEALER)
     private val loop: ZLoop = ZLoop(context)
 
-    fun requestEvaluate(functionName: String, blob: ByteArray): Unit = ZMsg().run {
-        add(byteArrayOf(T_REQUEST_EVALUATE))
+    class Expectation(
+        private val possibleCodes: ByteArray,
+        private val predicate: (ZMsg) -> Boolean,
+        private val callback: (ZMsg, Byte) -> Unit
+    ) {
+        operator fun component1(): ByteArray = possibleCodes
+        operator fun component2(): (ZMsg) -> Boolean = predicate
+        operator fun component3(): (ZMsg, Byte) -> Unit = callback
+    }
+
+    private val expectations: MutableList<Expectation> =
+        mutableListOf()
+
+    init {
+        val pollItem = ZMQ.PollItem(dealer, ZMQ.Poller.POLLIN)
+
+        loop.addPoller(pollItem, { _, _, _ ->
+            receive()
+            0
+        }, null)
+
+        loop.start()
+    }
+
+    private fun requestEvaluate(functionName: String, uuid: UUID, blob: ByteArray): Unit = ZMsg().run {
+        add(byteArrayOf(REQUEST_EVALUATE))
 
         add(ByteBuffer.allocate(16).run {
-            putUuid(UUID.randomUUID())
+            putUuid(uuid)
             array()
         })
 
@@ -28,16 +53,16 @@ internal class ClientTransport : Closeable {
         Unit
     }
 
-    fun requestCoderID(functionName: String) {
+    private fun requestCoderID(functionName: String) {
         ZMsg().run {
-            add(byteArrayOf(T_REQUEST_CODER_ID))
+            add(byteArrayOf(REQUEST_CODER_ID))
             add(functionName)
             send(dealer)
         }
     }
 
-    fun revoke(id: UUID): Unit = ZMsg().run {
-        add(byteArrayOf(T_REVOCATION))
+    private fun revoke(id: UUID): Unit = ZMsg().run {
+        add(byteArrayOf(REQUEST_REVOCATION))
 
         add(ByteBuffer.allocate(16).run {
             putUuid(id)
@@ -48,8 +73,61 @@ internal class ClientTransport : Closeable {
         Unit
     }
 
-    fun receive() {
-        loop.
+    private fun receive() {
+        val msg = ZMsg.recvMsg(dealer)
+        val type = msg.first.data[0]
+        msg.removeFirst()
+
+        val ri = expectations.asReversed().listIterator()
+
+        while (ri.hasNext()) {
+            val (t, p, c) = ri.next()
+
+            if (type in t && p(msg)) {
+                c(msg, type)
+                ri.remove()
+            }
+        }
+    }
+
+    fun evaluateAsync(functionName: String, blob: Payload): Deferred<Payload> {
+        val uuid = UUID.randomUUID()
+        requireNotNull(uuid)
+        requestEvaluate(functionName, uuid, blob)
+        val deferred = CompletableDeferred<ByteArray>()
+
+        expectations.add(Expectation(
+            byteArrayOf(11, 12, 13, 14),
+            { ByteBuffer.wrap(it.first.data).getUuid() == uuid }) { msg, type ->
+            revoke(uuid)
+            msg.removeFirst()
+
+            when (type) {
+                RESPONSE_SUCCESS -> deferred.complete(msg.first.data)
+                else -> error(msg.first.data.decodeToString())
+            }
+        })
+
+        return deferred
+    }
+
+    fun coderIDAsync(functionName: String): Deferred<UUID?> {
+        requestCoderID(functionName)
+        val deferred = CompletableDeferred<UUID?>()
+
+        expectations.add(Expectation(
+            byteArrayOf(21, 22),
+            { it.first.data.decodeToString() == functionName }) { msg, type ->
+            msg.removeFirst()
+
+            when (type) {
+                RESPONSE_FUNCTION_SUPPORTED -> deferred.complete(ByteBuffer.wrap(msg.first.data).getUuid())
+                RESPONSE_FUNCTION_UNSUPPORTED -> deferred.complete(null)
+                else -> error("Illegal code $type")
+            }
+        })
+
+        return deferred
     }
 
     override fun close() {
@@ -58,8 +136,15 @@ internal class ClientTransport : Closeable {
     }
 
     private companion object {
-        private const val T_REQUEST_EVALUATE: Byte = 11
-        private const val T_REQUEST_CODER_ID: Byte = 21
-        private const val T_REVOCATION: Byte = 31
+        private const val REQUEST_EVALUATE: Byte = 11
+        private const val REQUEST_CODER_ID: Byte = 21
+        private const val REQUEST_REVOCATION: Byte = 31
+        private const val RESPONSE_SUCCESS: Byte = 11
+        private const val RESPONSE_FUNCTION_EXCEPTION: Byte = 12
+        private const val RESPONSE_DECODING_EXCEPTION: Byte = 13
+        private const val RESPONSE_ENCODING_EXCEPTION: Byte = 14
+        private const val RESPONSE_FUNCTION_SUPPORTED: Byte = 21
+        private const val RESPONSE_FUNCTION_UNSUPPORTED: Byte = 22
+        private const val RESPONSE_RECEIVED: Byte = 31
     }
 }
---
+++ communicator-userapi/src/jvmMain/kotlin/scientifik/communicator/userapi/Client.kt
@@ -0,0 +1,37 @@
+package scientifik.communicator.userapi
+
+import kotlinx.coroutines.runBlocking
+import scientifik.communicator.api.Coder
+import scientifik.communicator.userapi.transport.ClientTransport
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+abstract class Client {
+    private val transport = ClientTransport()
+
+    fun <T> function(coder: Coder<T>): ReadOnlyProperty<Client, F<T>> = FunctionDelegate(coder)
+
+    private class FunctionDelegate<T>(private val coder: Coder<T>) : ReadOnlyProperty<Client, F<T>> {
+        override operator fun getValue(thisRef: Client, property: KProperty<*>): F<T> {
+            val name = property.name
+            var f: F<T>? = null
+
+            runBlocking {
+                val id = thisRef.transport.coderIDAsync(name).await()
+                requireNotNull(id)
+                f = F(name, thisRef, coder)
+            }
+
+            return f!!
+        }
+    }
+
+    class F<T> internal constructor(
+        private val name: String,
+        private val client: Client,
+        private val tCoder: Coder<T>
+    ) {
+        suspend operator fun invoke(arg: T): T =
+            tCoder.decode(client.transport.evaluateAsync(name, tCoder.encode(arg)).await())
+    }
+}
