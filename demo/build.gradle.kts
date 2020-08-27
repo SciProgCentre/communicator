07a268632c55332700e945ded94046e3fc348dec
Egor Zaitsev <zaitsev.eo@gmail.com>
2020-08-28 01:44:35 +0300

    fix

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,8 +1,29 @@
-plugins { id("scientifik.jvm") }
+import scientifik.useCoroutines
 
-dependencies {
-    implementation(project(":communicator-api"))
-    implementation(project(":communicator-zmq"))
-    implementation(project(":communicator-transport"))
-    implementation("org.slf4j:slf4j-simple:1.7.30")
+plugins { id("scientifik.mpp") }
+
+useCoroutines()
+
+kotlin.sourceSets {
+    all { languageSettings.useExperimentalAnnotation("kotlin.contracts.ExperimentalContracts") }
+
+    commonMain {
+        dependencies {
+            api(project(":communicator-api"))
+            api(project(":communicator-zmq"))
+            api(project(":communicator-transport"))
+            api("io.github.microutils:kotlin-logging-common:1.8.3")
+        }
+    }
+
+    jsMain { dependencies { api("io.github.microutils:kotlin-logging-js:1.8.3") } }
+
+    jvmMain {
+        dependencies {
+            api("io.github.microutils:kotlin-logging:1.8.3")
+            api("org.slf4j:slf4j-simple:1.7.30")
+            api("org.zeromq:jeromq:0.5.2")
+        }
+    }
+
 }
