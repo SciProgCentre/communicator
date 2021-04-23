5467fe0adc0e3d2ce52fd58f75703b71b516f495
Iaroslav Postovalov <postovalovya@gmail.com>
2021-04-23 22:54:01 +0700

    Use named Worker

--- communicator-zmq/src/nativeMain/kotlin/util/RunAsync.kt
+++ communicator-zmq/src/nativeMain/kotlin/util/RunAsync.kt
@@ -6,5 +6,5 @@ import kotlin.native.concurrent.Worker
 import kotlin.native.concurrent.freeze
 
 internal actual fun <T> runAsync(receiver: T, action: suspend (T) -> Unit): Any = Worker
-    .start()
+    .start(name = receiver.toString())
     .execute(TransferMode.SAFE, { receiver.freeze() to action.freeze() }) { (a, b) -> runBlocking { b(a) } }
